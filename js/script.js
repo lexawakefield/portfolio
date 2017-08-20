81de975d71950935673286ab354ffe7998de2378
lexawakefield <31116139+lexawakefield@users.noreply.github.com>
2017-08-20 16:33:44 -0400

    inital push
    Inital add of all files

---
+++ js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  // scroll to nav item
+  $('.nav_item').click(function(e) {
+    e.preventDefault();
+    var navId = $(this).data('scroll');
+    $("html, body").animate({scrollTop: $(navId).offset().top }, 1000);
+  });
+
+  $('nav').hide();
+
+  // if section top = window top show nav
+
+});
+
+
+
+//
+// Step 2: Scolling down needs to snap to the bottom of the next section.
+//
+// Step 3: The navigation needs to follow the user as the page scrolls vertically
+//
+// each section corresponds with a number in the navigation that should have an active state when the user is on the corresponding section
+//
